bd09538a8c5f32f7e0fbd8d9cde734275b881785
Benjamin Kästner <benjamin.kaestner@gmail.com>
2022-10-16 11:53:08 +0200

    outline: remove outline mode
    Unfortunately, `outline-minor-mode-cycle' breaks `tab` within
    programming languages and persists even if outline-minor-mode is
    disabled.

--- init.el
+++ init.el
@@ -100,10 +100,6 @@
   (setq desktop-restore-eager 0)
   (desktop-save-mode 1))
 
-(use-package outline
-  :hook (prog-mode . outline-minor-mode)
-  :custom (outline-minor-mode-cycle t))
-
 ;;; Helpers
 (use-package which-key
   :defer 1
